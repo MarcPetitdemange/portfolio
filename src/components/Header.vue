684bf4bfb5ddfeec90d1f565cd8ee8c9a9fe0ca4
Marc PETITDEMANGE <marc.petitdemange@ac-nancy-metz.fr>
2022-09-14 14:36:18 +0200

    Début de développement

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <i class="fa-brands fa-snapchat"></i>
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-discord"></i>
+        <i class="fa-brands fa-github"></i>
+    </header>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
